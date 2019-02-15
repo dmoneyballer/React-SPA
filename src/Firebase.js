b511ed6a5c06c5a35584b05ebaff1c1d4ec28bff
dmoneyballer <drew.gilliesbc@gmail.com>
2019-02-15 13:56:55 -0700

    update

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,17 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
-
-const config = {
-  apiKey: "AIzaSyCjC8FIAa2a_LMfXAkT3USUc0QKX_kBgXE",
-  authDomain: "react-spa-e8024.firebaseapp.com",
-  databaseURL: "https://react-spa-e8024.firebaseio.com",
-  projectId: "react-spa-e8024",
-  storageBucket: "react-spa-e8024.appspot.com",
-  messagingSenderId: "1045381734626"
-};
-firebase.initializeApp(config);
-
   export const provider = new firebase.auth.GoogleAuthProvider();
   export const auth = firebase.auth();
 
