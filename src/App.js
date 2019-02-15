f898195a29c1170117969b61cebcd194735a1609
dmoneyballer <drew.gilliesbc@gmail.com>
2019-02-15 11:40:16 -0700

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import Home from './Home.js';
-
+import Welcome from './Welcome';
+import Navigation from './Navigation';
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    }
+  }
   render() {
     return (
-      <Home />
+    <div>
+      <Navigation user={this.state.user} />
+      {this.state.user && <Welcome user={this.state.user}/>}
+      <Home user={this.state.user}/>
+    </div>
+      
     );
   }
 }
