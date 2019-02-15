bf771f708993aff3a8b0e44152883b040ef74619
dmoneyballer <drew.gilliesbc@gmail.com>
2019-02-15 13:35:52 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+// Import React
 import React, { Component } from 'react';
-import {Router} from '@reach/router';
+import { Router } from '@reach/router';
 import firebase from './Firebase';
 
 import Home from './Home';
@@ -9,42 +10,36 @@ import Login from './Login';
 import Register from './Register';
 import Meetings from './Meetings';
 
-
-
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
       user: null
-    }
+    };
   }
 
   componentDidMount() {
     const ref = firebase.database().ref('user');
 
-    ref.on("value", snapshot => {
+    ref.on('value', snapshot => {
       let FBUser = snapshot.val();
       this.setState({ user: FBUser });
-    })
+    });
   }
-
 
   render() {
     return (
-    <div>
-      <Navigation user={this.state.user} />
-      {this.state.user && <Welcome user={this.state.user}/>}
+      <div>
+        <Navigation user={this.state.user} />
+        {this.state.user && <Welcome user={this.state.user} />}
 
-      <Router>
-      <Home path="/" user={this.state.user}/>
-      <Login path="/login" />
-      <Meetings path="/meetings" />
-      <Register path="/register" />
-      </Router>
-      
-    </div>
-      
+        <Router>
+          <Home path="/" user={this.state.user} />
+          <Login path="/login" />
+          <Meetings path="/meetings" />
+          <Register path="/register" />
+        </Router>
+      </div>
     );
   }
 }
--- src/Register.js
+++ src/Register.js
@@ -1,106 +1,121 @@
-import React,  {Component} from 'react';
+import React, { Component } from 'react';
+import FormError from './FormError';
 
 class Register extends Component {
   constructor() {
     super();
     this.state = {
-      displayName: "",
-      email: "",
-      passOne: "",
-      passTwo: ""
+      displayName: '',
+      email: '',
+      passOne: '',
+      passTwo: '',
+      errorMessage: null
     };
+
     this.handleChange = this.handleChange.bind(this);
   }
-
 
   handleChange(e) {
     const itemName = e.target.name;
     const itemValue = e.target.value;
-    this.setState({ [itemName]: itemValue})
+
+    this.setState({ [itemName]: itemValue }, () => {
+      if (this.state.passOne !== this.state.passTwo) {
+        this.setState({ errorMessage: 'Passwords no not match' });
+      } else {
+        this.setState({ errorMessage: null });
+      }
+    });
   }
-  
-    render() {  
-        return(
-          <form className="mt-3">
-          <div className="container">
-            <div className="row justify-content-center">
-              <div className="col-lg-8">
-                <div className="card bg-light">
-                  <div className="card-body">
-                    <h3 className="font-weight-light mb-3">Register</h3>
-                    <div className="form-row">
-                      <section className="col-sm-12 form-group">
-                        <label
-                          className="form-control-label sr-only"
-                          htmlFor="displayName"
-                        >
-                              Display Name
-                            </label>
-                        <input
-                          className="form-control"
-                          type="text"
-                          id="displayName"
-                          placeholder="Display Name"
-                          name="displayName"
-                          required
-                          value={this.state.displayName}
-                          onChange={this.handleChange}
-                        />
-                      </section>
-                    </div>
-                    <section className="form-group">
+
+  render() {
+    return (
+      <form className="mt-3">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8">
+              <div className="card bg-light">
+                <div className="card-body">
+                  <h3 className="font-weight-light mb-3">Register</h3>
+                  <div className="form-row">
+                    {this.state.errorMessage !== null ? (
+                      <FormError
+                        theMessage={this.state.errorMessage}
+                      />
+                    ) : null}
+                    <section className="col-sm-12 form-group">
                       <label
                         className="form-control-label sr-only"
-                        htmlFor="email"
+                        htmlFor="displayName"
                       >
-                        Email
+                        Display Name
                       </label>
                       <input
                         className="form-control"
-                        type="email"
-                        id="email"
-                        placeholder="Email Address"
+                        type="text"
+                        id="displayName"
+                        placeholder="Display Name"
+                        name="displayName"
                         required
-                        name="email"
-                        value={this.state.email}
+                        value={this.state.displayName}
                         onChange={this.handleChange}
                       />
                     </section>
-                    <div className="form-row">
-                      <section className="col-sm-6 form-group">
-                        <input
-                          className="form-control"
-                          type="password"
-                          name="passOne"
-                          placeholder="Password"
-                          value={this.state.passOne}
+                  </div>
+                  <section className="form-group">
+                    <label
+                      className="form-control-label sr-only"
+                      htmlFor="email"
+                    >
+                      Email
+                    </label>
+                    <input
+                      className="form-control"
+                      type="email"
+                      id="email"
+                      placeholder="Email Address"
+                      required
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                    />
+                  </section>
+                  <div className="form-row">
+                    <section className="col-sm-6 form-group">
+                      <input
+                        className="form-control"
+                        type="password"
+                        name="passOne"
+                        placeholder="Password"
+                        value={this.state.passOne}
                         onChange={this.handleChange}
-                        />
-                      </section>
-                      <section className="col-sm-6 form-group">
-                        <input
-                          className="form-control"
-                          type="password"
-                          required
-                          name="passTwo"
-                          placeholder="Repeat Password"
-                          value={this.state.passTwo}
+                      />
+                    </section>
+                    <section className="col-sm-6 form-group">
+                      <input
+                        className="form-control"
+                        type="password"
+                        required
+                        name="passTwo"
+                        placeholder="Repeat Password"
+                        value={this.state.passTwo}
                         onChange={this.handleChange}
-                        />
-                      </section>
-                    </div>
-                    <div className="form-group text-right mb-0">
-                      <button className="btn btn-primary" type="submit">
-                        Register
-                      </button>
-                    </div>
+                      />
+                    </section>
+                  </div>
+                  <div className="form-group text-right mb-0">
+                    <button className="btn btn-primary" type="submit">
+                      Register
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </form>
-        )
-    }
+        </div>
+      </form>
+    );
+  }
 }
+
 export default Register;
