a0845cdb6b46ca918b7badfde162410fd74ecb8c
dmoneyballer <drew.gilliesbc@gmail.com>
2019-02-16 09:31:11 -0700

    added fire

---
+++ src/Firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+  export const provider = new firebase.auth.GoogleAuthProvider();
+  export const auth = firebase.auth();
+
+<<<<<<< HEAD
+export default firebase;
+=======
+export default firebase;
+>>>>>>> 51e476ce1f7454272725af52a68c5d6dd1e71bc6
