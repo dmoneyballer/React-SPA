d8460f79ef16a0df81a207cf3d81a4f804e95f0a
dmoneyballer <drew.gilliesbc@gmail.com>
2019-02-16 17:35:34 -0700

    finished product

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
+
+// firebase api goes here
+firebase.initializeApp(config);
   export const provider = new firebase.auth.GoogleAuthProvider();
   export const auth = firebase.auth();
 
